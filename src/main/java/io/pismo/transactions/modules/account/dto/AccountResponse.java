a05d98a13d3df5e9a435e3e58aecdbad99555afd
hivana <6864934+hivana@users.noreply.github.com>
2022-05-11 12:08:48 -0300

    Update AccountResponse.java

--- src/main/java/io/pismo/transactions/modules/account/dto/AccountResponse.java
+++ src/main/java/io/pismo/transactions/modules/account/dto/AccountResponse.java
@@ -3,7 +3,6 @@ package io.pismo.transactions.modules.account.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-//@JsonSerialize
 public record AccountResponse(
         @JsonProperty("account_id") Long id,
         @JsonProperty("document_number") Long documentNumber) {
